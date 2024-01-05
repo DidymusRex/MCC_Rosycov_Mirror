b37ce837d366350c54b80c39c5ec32091c2b9bf2
John Grover <d.rex@didymusrex.net>
2024-01-04 20:28:37 -0500

    local gitignore fix

--- console/config.py
+++ console/config.py
@@ -1,6 +1,12 @@
 # config.py
+SSID='grover_net'
+SSID_PW='open says me'
 
-mqtt_broker = "mcc-cthulu.local"
-mqtt_port = 1883
-mqtt_subscribe_topic = "mcc/status"
-mqtt_publish_topic = "mcc/event"
+mqtt_username='mcc'
+mqtt_password='mcc'
+mqtt_broker = 'mcc-cthulu.local'
+mqtt_port=1883
+mqtt_client_id = 'mcc-p1'
+mqtt_subscribe_topic = b'mcc/p1/status/#'
+mqtt_publish_topic = b'mcc/p1/event/#'
+
